7bbbdf24874fb2b024f772a73c7f11c5b1e66b34
Yefridev <yefri.dev@gmail.com>
2026-06-16 21:36:02 -0500

    feat:coregir prueba de lectura de correos no leidos

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from src.gmail.reader import obtener_correos_no_leidos
 if __name__ == "__main__":
     servicio = obtener_servicio_gmail()
     correos = obtener_correos_no_leidos(servicio)
-#    print("Conexión exitosa con Gmail")
 
     for correo in correos:
         print("---")
